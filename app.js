e439b982bfe3ab320152a093c993a38c8dfafe47
garimayadav01 <garimyadav933@gmail.com>
2024-02-14 17:49:30 +0530

    connect with mongodb and scures and make sever also......

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const connectDB = require("./db/connect")
 const PORT = process.env.PORT || 3000;
 
 const products_routes = require("./routes/product")
@@ -8,9 +9,14 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.use("/api/products",products_routes);
+app.use("/api/products", products_routes);
 
 // Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, async () => {
+    try {
+        await connectDB();
+        console.log(`Server is running on port ${PORT}`);
+    } catch (error) {
+        console.error('Error connecting to the database:', error.message);
+    }
 });
---
+++ db/connect.js
@@ -0,0 +1,14 @@
+
+const mongoose = require("mongoose");
+
+uri = "mongodb+srv://garimyadav933:F3xvIdUzKWgc1S5j@cluster0.9x9gczf.mongodb.net/MYAPP?retryWrites=true&w=majority";
+
+const connectDB = () => {
+console.log("connect data base")
+    return mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+};
+
+module.exports = connectDB;
